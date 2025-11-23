661283ef7c7a2f477b8f3e2419dd1a4139f88d35
Nikola Ubavic <53820106+ubavic@users.noreply.github.com>
2025-11-23 17:47:56 +0100

    Reorganize main a little bit

--- cmd/main.go
+++ cmd/main.go
@@ -16,36 +16,55 @@ func main() {
 	inputFileFlag := flag.String("in", "", "Specifies a input file")
 	schemaFileFlag := flag.String("schema", "", "Specifies a schema file")
 	targetFlag := flag.String("target", "", "Select target from schema")
+	outputFileFlag := flag.String("out", "", "Specifies a output file")
+	jsonFlag := flag.Bool("json", false, "Output JSON")
+
 	flag.Parse()
 
-	if *inputFileFlag == "" {
-		fmt.Println("Expected input file")
-		return
+	config := Config{
+		InputFile:  *inputFileFlag,
+		SchemaFile: *schemaFileFlag,
+		Target:     *targetFlag,
+		Json:       *jsonFlag,
+		OutputFile: *outputFileFlag,
 	}
 
-	if *schemaFileFlag == "" {
-		fmt.Println("Expected schema file")
-		return
-	}
-
-	schemaFile, err := os.ReadFile(*schemaFileFlag)
+	err := process(config)
 	if err != nil {
-		fmt.Printf("Can't open file \"%s\": %v", *schemaFileFlag, err.Error())
-		return
+		printErrorAndExit(err)
+	}
+}
+
+type Config struct {
+	InputFile  string
+	SchemaFile string
+	Target     string
+	Json       bool
+	OutputFile string
+}
+
+func process(config Config) error {
+	if config.InputFile == "" {
+		return fmt.Errorf("expected input file")
 	}
 
-	file, err := os.Open(*inputFileFlag)
+	file, err := os.Open(config.InputFile)
 	if err != nil {
-		fmt.Printf("Can't open file \"%s\": %v", *inputFileFlag, err.Error())
-		return
+		return fmt.Errorf("can't open file \"%s\": %v", config.InputFile, err.Error())
 	}
 
-	var newSchema schema.Schema
+	var newSchema *schema.Schema = nil
 
-	err = yaml.Unmarshal(schemaFile, &newSchema)
-	if err != nil {
-		fmt.Printf("Can't unmarshal schema: %v", err.Error())
-		return
+	if config.SchemaFile != "" {
+		schemaFile, err := os.ReadFile(config.SchemaFile)
+		if err != nil {
+			return fmt.Errorf("can't open file \"%s\": %v", config.SchemaFile, err.Error())
+		}
+
+		err = yaml.Unmarshal(schemaFile, &newSchema)
+		if err != nil {
+			return fmt.Errorf("can't unmarshal schema: %v", err.Error())
+		}
 	}
 
 	fileBuf := bufio.NewReader(file)
@@ -56,17 +75,43 @@ func main() {
 	parser := parser.NewParser(tokens, newSchema)
 	doc, err := parser.Parse()
 	if err != nil {
-		fmt.Printf("Error while parsing \"%s\": %v", *inputFileFlag, err.Error())
-		return
+		return fmt.Errorf("error while parsing \"%s\": %v", config.InputFile, err.Error())
 	}
 
-	target, err := newSchema.GetTarget(*targetFlag)
-	if err != nil {
-		fmt.Printf("Can't find target: %v", err.Error())
-		return
+	var target *schema.Target = nil
+	if config.Target != "" {
+
+		if newSchema == nil {
+			return fmt.Errorf("schema is not loaded")
+		}
+
+		target, err = newSchema.GetTarget(config.Target)
+		if err != nil {
+			return fmt.Errorf("can't find target: %v", err.Error())
+		}
 	}
 
-	rendered := writer.Write(target, doc)
+	rendered := ""
 
-	fmt.Println(rendered)
+	if config.Json {
+		rendered = string(doc.Json())
+	} else {
+		rendered = writer.Write(target, doc)
+	}
+
+	if config.OutputFile != "" {
+		err = os.WriteFile(config.OutputFile, []byte(rendered), 0644)
+		if err != nil {
+			return fmt.Errorf("can't write to file \"%s\": %v", config.OutputFile, err.Error())
+		}
+	} else {
+		fmt.Println(rendered)
+	}
+
+	return nil
+}
+
+func printErrorAndExit(err error) {
+	fmt.Println("\x1b[91m" + err.Error() + "\x1b[0m")
+	os.Exit(1)
 }
